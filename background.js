4926640cb3178bf5fcab2b74fe6937baa0e58d49
petetalksweb <jacob@jacob-petersen.com>
2017-03-27 17:12:31 -0500

    Changing from onclick to onclicked, and actually saving to list via context menus

--- background.js
+++ background.js
@@ -1,14 +1,15 @@
 chrome.contextMenus.create({
     "title": "Add to read later list",
-    "contexts": ["page"],
-    "onclick": function() {
-        alert('yo yo yo');
-    }
+    "contexts": ["page"]
 });
 chrome.contextMenus.create({
     "title": "Add link to read later list",
-    "contexts": ["link"],
-    "onclick": function() {
-        alert('yo yo yo');
+    "contexts": ["link"]
+});
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
+    if(info.linkUrl) {
+        addToReadList(info.linkUrl, info.linkUrl);
+    } else {
+        addToReadList(tab.url, tab.title);
     }
 });
