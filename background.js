4109dffa4ad8b42551846701db0893918f454222
jacobpetersen <jacob@jacob-petersen.com>
2017-03-28 00:26:41 -0500

    Adding selection context menu

--- background.js
+++ background.js
@@ -6,9 +6,16 @@ chrome.contextMenus.create({
     "title": "Add link to read later list",
     "contexts": ["link"]
 });
+chrome.contextMenus.create({
+    "title": "Add selection to snippets list",
+    "contexts": ["selection"]
+});
 chrome.contextMenus.onClicked.addListener(function (info, tab) {
     if(info.linkUrl) {
         addToReadList(info.linkUrl, info.linkUrl);
+    } else if(info.selectionText) {
+        alert(info.selectionText);
+        addToSnippetsList(tab.url, tab.title, info.selectionText);
     } else {
         addToReadList(tab.url, tab.title);
     }
